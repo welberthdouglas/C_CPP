53d01e19b87d30d9f8d0d24b478ae7ee16b40c7a
welberthdouglas <welberthdouglas@gmail.com>
2023-04-16 18:01:43 -0400

    bubble sort c

---
+++ bubble.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp; 
+}
+
+void bubble(int arr[], int size) {
+    int last = size - 1;
+
+    while (last > 0) {
+        for (int i = 0; i < last ; i++) {
+            if (arr[i] > arr[i+1]) {
+                swap(&arr[i], &arr[i+1]);
+            }
+        }
+        last--;
+    }
+}
+
+int main(void) {
+    int arr[12] = {1, 10, 23, 8, 0, -7, 89, -5, 6, 33, 9890, -98976};
+    bubble(arr, sizeof(arr)/sizeof(int));
+
+    for (int i=0; i < sizeof(arr)/sizeof(int); i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
